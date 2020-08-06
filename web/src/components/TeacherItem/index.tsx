90d25b4c8019ae393df16395c5ce1c806ec99316
Richard Felipe Da Silva Oliveira <richardfelipe.123@hotmail.com>
2020-08-06 00:23:29 -0300

    Web Back-Frontend Finalizado
    Parte Web Finalizada

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,32 +3,51 @@ import React from 'react';
 import whatsappIcon from'../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher{
+    id: number,
+    subject: string,
+    cost: number
+    name: string,
+    avatar: string,
+    whatsapp: string,
+    bio: string
+}
+
+export interface TeacherItemProps {
+    teacher:Teacher
+}
+
+const TeacherItem: React.FC <TeacherItemProps>=({teacher})=>{
+    function createNewConnection(){
+        api.post('connections',{
+            user_id:teacher.id,
+        })
+    }
     return(
         <article className="teacher-item">
                 <header>
                 
-                <img src="https://avatars3.githubusercontent.com/u/54228659?s=460&u=b1c1dfca284dadae66ca13d86e52edb0813d0c02&v=4" alt="Richard Felipe"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Richard Felipe</strong>
-                    <span>DevOps</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
                 </header>   
                 <p>
-                    Entusiasta das melhores e mais dificeis linguagens de programação
-                    <br/><br/>
-                    Apaixonado por explodir computadores, digitando códigos malucos.
+                    {teacher.bio}
+                    
                 </p>
                 <footer>
                     <p>
                         Preço/hora
-                        <strong>R$90.00</strong>
+                        <strong>{teacher.cost}</strong>
                     </p>
-                    <button type="button">
+                    <a target="_blank" onClick={createNewConnection}href={`https://wa.me/${teacher.whatsapp}`}>
                         <img src={whatsappIcon} alt="Whatsapp" />
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
                 </article>
     )
